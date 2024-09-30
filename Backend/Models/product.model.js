511fdcd650b1c051d1d37f496ff1a5943581039d
orestengabo0 <orestengabo0@gmail.com>
2024-09-30 15:28:53 +0200

    Creating a product schema

---
+++ Backend/Models/product.model.js
@@ -0,0 +1,70 @@
+const { required, string, ref } = require('joi')
+const mongoose = require('mongoose')
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    images: [
+        {
+            url: {
+                type: String,
+                required: true
+            },
+            altText: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    brand: {
+        type: String,
+        trim: true
+    },
+    rating: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            rating: {
+                type: Number,
+                min: 1,
+                max: 5
+            },
+            comment: {
+                type: String,
+                trim: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
