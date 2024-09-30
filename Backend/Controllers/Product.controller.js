e0deef4a8f571d6b89464413ac855a83aad3e0c8
orestengabo0 <orestengabo0@gmail.com>
2024-09-30 20:58:48 +0200

    creating a route for getting all the products and a single product

---
+++ Backend/Controllers/Product.controller.js
@@ -0,0 +1,54 @@
+const validateCreateProduct = require("../Validation/validateProduct");
+const { Product } = require("../Models/product.model");
+
+const createNewProduct = async (req, res) => {
+    const { error } = validateCreateProduct(req.body);
+    if (error)
+      return res
+        .status(400)
+        .json({ success: false, message: error.details[0].message });
+    try {
+      const { name, description, price, category, stock, images, ratings } = req.body;
+      const newProduct = new Product({
+        name,
+        description,
+        price,
+        category,
+        stock,
+        images,
+        ratings
+      });
+      await newProduct.save()
+      res.status(201).json({success: true, message: "Product created successfully."})
+    } catch (error) {
+      console.error("Error creating user ", error)
+      res.status(500).json({success:false, message: "Server error."})
+    }
+}
+
+const getAllProducts = async (req, res) => {
+    try {
+      const products = await Product.find();
+      if (!products)
+        return res
+          .status(404)
+          .json({ success: false, message: "Product not found." });
+      res.status(200).json({success: true, data: products})
+    } catch (error) {
+      res.status(500).json({success: false, message: "Server error."})
+    }
+  }
+
+const getProduct = async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (!product)
+        return res
+          .status(404)
+          .json({ succes: false, message: "Product not found." });
+      res.status(200).json({succes: true, data: product})
+    } catch (error) {
+      res.status(500).json({succes: false, message: "Server error."})
+    }
+  }
+module.exports = { createNewProduct, getAllProducts, getProduct }
--- Backend/Routes/product.router.js
+++ Backend/Routes/product.router.js
@@ -1,32 +1,21 @@
 const express = require("express");
-const validateCreateProduct = require("../Validation/validateProduct");
-const { Product } = require("../Models/product.model");
 const { authenticate, authorize } = require("../Middlewares/authentication");
+const {
+  createNewProduct,
+  getAllProducts,
+  getProduct,
+} = require("../Controllers/Product.controller");
+const { Product } = require("../Models/product.model");
 const productRoute = express.Router();
 
-productRoute.post("/create", authenticate, authorize(['admin']), async (req, res) => {
-  const { error } = validateCreateProduct(req.body);
-  if (error)
-    return res
-      .status(400)
-      .json({ success: false, message: error.details[0].message });
-  try {
-    const { name, description, price, category, stock, images, ratings } = req.body;
-    const newProduct = new Product({
-      name,
-      description,
-      price,
-      category,
-      stock,
-      images,
-      ratings
-    });
-    await newProduct.save()
-    res.status(201).json({success: true, message: "Product created successfully."})
-  } catch (error) {
-    console.error("Error creating user ", error)
-    res.status(500).json({success:false, message: "Server error."})
-  }
-});
+productRoute.get("/", getAllProducts);
+productRoute.get("/:id", getProduct);
+productRoute.post(
+  "/create",
+  authenticate,
+  authorize(["admin"]),
+  createNewProduct
+);
+productRoute.delete("/delete/:id", );
 
-module.exports = productRoute
+module.exports = productRoute;
