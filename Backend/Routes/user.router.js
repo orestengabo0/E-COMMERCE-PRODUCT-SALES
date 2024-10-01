205b9fdffbd6b7801362ea2ecda4536811cc86e0
orestengabo0 <orestengabo0@gmail.com>
2024-10-01 13:29:07 +0200

    protecting get route

--- Backend/Routes/user.router.js
+++ Backend/Routes/user.router.js
@@ -12,8 +12,8 @@ const { authenticate, authorize } = require("../Middlewares/authentication");
 const { User } = require("../Models/user.model");
 const userRoute = express.Router();
 
-userRoute.get("/users", getAllUsers)
-userRoute.get("/users/:id", authenticate, authorize(['admin']), getUserById);
+userRoute.get("/users", authenticate, authorize(["admin"]), getAllUsers);
+userRoute.get("/users/:id", authenticate, authorize(["admin"]), getUserById);
 userRoute.get("/users/me", authenticate, getCurrentUser);
 userRoute.post("/register", createUser);
 userRoute.post("/login", loginUser);
