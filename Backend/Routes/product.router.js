d923920d226e2e46e79e027b417e454821f5e3bf
orestengabo0 <orestengabo0@gmail.com>
2024-10-01 07:04:18 +0200

    refactoring the update product route

--- Backend/Routes/product.router.js
+++ Backend/Routes/product.router.js
@@ -5,9 +5,8 @@ const {
   getAllProducts,
   getProduct,
   deleteProduct,
+  updateProduct,
 } = require("../Controllers/Product.controller");
-const { Product } = require("../Models/product.model");
-const { validateUpdateProduct } = require("../Validation/validateProduct");
 const productRoute = express.Router();
 
 productRoute.get("/", getAllProducts);
@@ -24,32 +23,6 @@ productRoute.delete(
   authorize(["admin"]),
   deleteProduct
 );
-productRoute.put("/update/:id", async (req, res) => {
-  const { error } = validateUpdateProduct(req.body);
-  if (error)
-    return res
-      .status(400)
-      .json({ success: false, message: error.details[0].message });
-  try {
-    const { id } = req.params;
-    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!updatedProduct)
-      return res
-        .status(404)
-        .json({ success: false, message: "Product not found." });
-    res
-      .status(200)
-      .json({
-        success: false,
-        data: updatedProduct,
-        message: "Product updated successfully.",
-      });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server error." });
-  }
-});
+productRoute.put("/update/:id", updateProduct);
 
 module.exports = productRoute;
