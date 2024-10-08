8685d66710269751b6d4276ada4e4500a897cde4
orestengabo0 <orestengabo0@gmail.com>
2024-10-08 11:19:25 +0200

    Adding an update category route

--- Backend/Controllers/Category.controller.js
+++ Backend/Controllers/Category.controller.js
@@ -2,22 +2,48 @@ const { validateCategory } = require("../Validation/validateCategory");
 const Category = require("../Models/category.model");
 
 const createCategory = async (req, res) => {
-    const { error } = validateCategory(req.body);
-    if (error)
-      return res
-        .status(400)
-        .json({ success: false, message: error.details[0].message });
-    try {
-      const { name } = req.body;
-      const newCategory = new Category({
-        name: name,
-      });
-      await newCategory.save();
-      res
-        .status(201)
-        .json({ success: true, message: "Category created successfully." });
-    } catch (error) {
-      res.status(500).json({ success: false, message: "Server error." });
-    }
+  const { error } = validateCategory(req.body);
+  if (error)
+    return res
+      .status(400)
+      .json({ success: false, message: error.details[0].message });
+  try {
+    const { name } = req.body;
+    const newCategory = new Category({
+      name: name,
+    });
+    await newCategory.save();
+    res
+      .status(201)
+      .json({ success: true, message: "Category created successfully." });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error." });
   }
-module.exports = { createCategory }
+};
+const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find()
+        if(!categories) return res.status(404).json({ success: false, message: "No category found."})
+        res.status(200).json({ success: true, message: "Successfully.", categories})
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error."})
+    }
+}
+
+const updateCategory = async (req, res)=> {
+    const { error } = validateCategory(req.body)
+    if(error) return res.status(400).json({ success: false, message: error.details[0].message})
+    try {
+        const categoryId = req.params.categoryId
+        const { name } = req.body
+        const category = await Category.findById(categoryId)
+        if(!category)
+            return res.status(404).json({ success: false, message: "No category found."})
+        category.name = name
+        await category.save()
+        res.status(200).json({ success: true, message: "Category update successfully.", category})
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error."})
+    }
+}
+module.exports = { createCategory, getCategories, updateCategory };
--- Backend/Routes/category.router.js
+++ Backend/Routes/category.router.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const { authenticate, authorize } = require("../Middlewares/authentication");
-const { createCategory } = require("../Controllers/Category.controller");
+const {
+  createCategory,
+  getCategories,
+  updateCategory,
+} = require("../Controllers/Category.controller");
 const Category = require("../Models/category.model");
+const { validateCategory } = require("../Validation/validateCategory");
 const categoryRoute = express.Router();
-categoryRoute.get("/", async (req, res) => {
-    try {
-        const categories = await Category.find()
-        if(!categories) return res.status(404).json({ success: false, message: "No category found."})
-        res.status(200).json({ success: true, message: "Successfully.", categories})
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Server error."})
-    }
-})
+categoryRoute.get("/", authenticate, getCategories);
 categoryRoute.post(
   "/create",
   authenticate,
   authorize(["admin"]),
   createCategory
+);
+categoryRoute.put(
+  "/update/:categoryId",
+  authenticate,
+  authorize(["admin"]),
+  updateCategory
 );
 module.exports = categoryRoute;
