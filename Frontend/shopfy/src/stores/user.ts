a5523815714556d83be1685a5a74d1710bb6c066
orestengabo0 <orestengabo0@gmail.com>
2024-10-13 18:28:57 +0200

    creating a creating message function to user hook

--- Frontend/shopfy/src/stores/user.ts
+++ Frontend/shopfy/src/stores/user.ts
@@ -9,31 +9,39 @@ interface User {
 }
 
 interface UserLogin {
-    email: string,
-    password: string
+  email: string;
+  password: string;
 }
 
 interface DecodeToken {
-    id: string,
-    role: string
+  id: string;
+  role: string;
 }
 
-interface ApiResponse {
-  success: boolean;
+interface Message {
+  name: string;
+  email: string;
   message: string;
 }
 
 interface UserState {
   users: User[];
+  messages: Message[];
   isLogggedIn: boolean;
   role: string | null;
   createUser: (user: User) => Promise<{ success: boolean; message: string }>;
-  loginUser: (user: UserLogin) => Promise<{ success: boolean; message: string, role?: string}>;
-  logoutUser: () => void
+  loginUser: (
+    user: UserLogin
+  ) => Promise<{ success: boolean; message: string; role?: string }>;
+  logoutUser: () => void;
+  createMessage: (
+    message: Message
+  ) => Promise<{ success: boolean; message: string }>;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   users: [],
+  messages: [],
   isLogggedIn: false,
   role: null,
   createUser: async (user) => {
@@ -80,10 +88,10 @@ export const useUserStore = create<UserState>((set) => ({
       );
       if (res.data.success) {
         const { token } = res.data;
-        localStorage.setItem("authToken", token)
-        const decodedToken: DecodeToken = jwtDecode(token)
-        const { role } = decodedToken
-        set({ isLogggedIn: true, role})
+        localStorage.setItem("authToken", token);
+        const decodedToken: DecodeToken = jwtDecode(token);
+        const { role } = decodedToken;
+        set({ isLogggedIn: true, role });
         return { success: true, message: "Login successful.", role };
       }
       return { success: false, message: "Login failed." };
@@ -92,7 +100,29 @@ export const useUserStore = create<UserState>((set) => ({
     }
   },
   logoutUser: () => {
-    localStorage.removeItem("authToken")
-    set({ isLogggedIn: false, users: []})
-  }
+    localStorage.removeItem("authToken");
+    set({ isLogggedIn: false, users: [] });
+  },
+  createMessage: async (message) => {
+    if (!message.name || !message.email || !message.message)
+      return { success: false, message: "Please fill in all fields." };
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/messages/send",
+        message,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.data.success) {
+        set((state) => ({ messages: [...state.messages, message] }));
+        return { success: true, message: "Message sent successfully." };
+      }
+      return { success: false, message: "Failed to send Message." };
+    } catch (error) {
+      return { success: false, message: "Something went wrong." };
+    }
+  },
 }));
