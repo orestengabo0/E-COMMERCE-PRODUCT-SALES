a1cdcf9dd5b8b33c486e45dbc38e4419af4ed815
orestengabo0 <orestengabo0@gmail.com>
2024-10-18 11:36:11 +0200

    adding a schema for storing address of a user

---
+++ Backend/Models/address.router.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const addressSchema = new mongoose.Schema(
+  {
+    name: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    City: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ZipCode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isDefault: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const Address = mongoose.model("Address", addressSchema);
+module.exports = Address;
