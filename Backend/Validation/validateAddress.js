6f5013dc9acb5250edabcbbf5b4983f13cad9e1c
orestengabo0 <orestengabo0@gmail.com>
2024-10-18 11:43:08 +0200

    Adding a function to validate user profile on updating

--- Backend/Validation/validateAddress.js
+++ Backend/Validation/validateAddress.js
@@ -1,10 +1,24 @@
-const Joi = require('joi')
+const Joi = require("joi");
 const validateUserAddress = (address) => {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        street: Joi.string().required(),
-        City: Joi.string().required(),
-        ZipCode: Joi.string().required(),
-        Country: Joi.string().required()
-    })
-}
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    street: Joi.string().required(),
+    City: Joi.string().required(),
+    ZipCode: Joi.string().required(),
+    Country: Joi.string().required(),
+  });
+  return schema.validate(address);
+};
+
+const validateUpdateUserAddress = (address) => {
+  const schema = Joi.object({
+    name: Joi.string().optional(),
+    street: Joi.string().optional(),
+    City: Joi.string().optional(),
+    ZipCode: Joi.string().optional(),
+    Country: Joi.string().optional(),
+  });
+  return schema.validate(address);
+};
+
+module.exports = { validateUserAddress, validateUpdateUserAddress };
